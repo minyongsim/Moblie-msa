7bdedb5f1e479bd05a7b09a1684131c5f0c8dbbc
minyongsim <68522496+minyongsim@users.noreply.github.com>
2020-10-21 18:11:23 +0900

    ㅇㅇㅇㅇㅇ

--- mobile_msa.js
+++ mobile_msa.js
@@ -1,1 +1,25 @@
-(function(){})(jQuery)
+(function(){
+
+    $('#header .open_nav').on('click', function () {
+        $(this).next().stop().css({
+            display:'block'
+            }).animate({
+                left:'0px'
+            },500)
+        $(this).hide()
+        $(this).nextAll('.close_nav').show()
+    })
+    $('#header .close_nav').on('click', function () {
+        $(this).prevAll('.menubox').stop().css({
+            display:'none'
+            }).animate({
+                left:'0px'
+            },500)
+        $(this).hide()
+        $(this).prevAll('.open_nav').show()
+     
+    })
+
+
+
+})(jQuery)
